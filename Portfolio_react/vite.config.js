82634cb4bea29a462da825e89492ee738f95f026
Dass23M <157539720+Dass23M@users.noreply.github.com>
2025-04-24 23:36:02 +0530

    add repository name

--- Portfolio_react/vite.config.js
+++ Portfolio_react/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Portfolio/', // <-- replace with your actual repo name
+  base: '/Portfolio/', 
   plugins: [react()],
 })
 
